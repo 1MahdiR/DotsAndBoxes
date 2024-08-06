371831dceffc03e316d0f4ea67fb3291f53d90af
MR <a.m.rasouli.n@gmail.com>
2024-08-06 04:59:28 +0330

    Added reward function

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
+from copy import deepcopy
 
-n = 5 # width
-m = 7 # height
+n = 4 # width
+m = 3 # height
 
 class Edge:
     def __init__(self, d, x, y, c):
@@ -23,22 +24,13 @@ class Square:
         self.ls = []
 
     def __repr__(self):
-        return "<Square: x=%d, y=%d, r=%d, edges=%s>" % (self.x, self.y, self.r, str(self.ls))
+        return "<Square: x=%s, y=%d, r=%s, edges=%s>" % (self.x, self.y, str(self.r), str(self.ls))
     
     def __str__(self):
         return self.__repr__()
     
-def draw_board(board_x, board_y, board_p):
-    ls = []
-    i_x = 0
-    i_y = 0
-    for i in range(len(board_x) + len(board_y)):
-        if i % 2 == 0:
-            ls.append(board_x[i_x])
-            i_x += 1
-        else:
-            ls.append(board_y[i_y])
-            i_y += 1
+def draw_board(edges, board_p):
+    ls = edges
     
     table = []
 
@@ -79,6 +71,114 @@ def draw_board(board_x, board_y, board_p):
     for i in table:
         print(i)
 
+def calculate_reward(board_p:list, edge:Edge):
+    edge.c = 1
+    
+    changed_squares = []
+    reward = 0
+    for i in board_p:
+        for j in i:
+            if j.r == 0:
+                SQUARED = True
+                for k in j.ls:
+                    if k.c == 0:
+                        SQUARED = False
+                if SQUARED:
+                    j.r = "X"
+                    reward += 1
+                    changed_squares.append(j)
+    
+    edge.c = 0
+    for i in changed_squares:
+        i.r = 0
+
+    return reward
+
+def calculate_regret(board_p:list, edge:Edge):
+    edge.c = 1
+    
+    changed_squares = []
+    regret = 0
+    for i in board_p:
+        for j in i:
+            if j.r == 0:
+                SQUARED = True
+                for k in j.ls:
+                    if k.c == 0:
+                        SQUARED = False
+                if SQUARED:
+                    j.r = "O"
+                    regret += 1
+                    changed_squares.append(j)
+    
+    edge.c = 0
+    for i in changed_squares:
+        i.r = 0
+
+    return regret
+
+def calculate_reward_regret_0_layer(edges, board_p:list, depth=0, turn=True, reward=0):
+    max_reward = (0, None)
+
+    ls = edges
+    current_depth = depth
+    current_turn = turn
+
+    if depth == 3:
+        for i in ls:
+            for j in i:
+                if j.c == 0:
+                    r = calculate_reward(board_p, j)
+                    if r > max_reward[0]:
+                        max_reward = (r, j)
+                        #print("k", max_reward)
+        if turn:
+            #print(depth, max_reward, turn)
+            return max_reward
+        else:
+            #print(depth, (-max_reward[0], max_reward[1]), turn)
+            return (-max_reward[0], max_reward[1])
+
+    r_ls = []
+    for i in ls:
+        for j in i:
+            if j.c == 0:
+                re = calculate_reward(board_p, j)
+                #print(re)
+                if re:
+                    j.c = 1
+                    changed_squares = []
+                    for k in board_p:
+                        for l in k:
+                            if l.r == 0:
+                                SQUARED = True
+                                for m in l.ls:
+                                    if m.c == 0:
+                                        SQUARED = False
+                                if SQUARED:
+                                    m.r = "X"
+                                    changed_squares.append(l)
+                    reward = calculate_reward_regret_0_layer(ls, board_p, depth=current_depth+1, turn=(current_turn))[0]
+                    for m in changed_squares:
+                        m.r = 0
+                    j.c = 0
+                else:
+                    j.c = 1
+                    reward = calculate_reward_regret_0_layer(ls, board_p, depth=current_depth+1, turn=(not current_turn))[0]
+                    j.c = 0
+
+                r_ls.append((reward, j))
+    if turn:
+        m = max(r_ls, key=lambda x: x[0])
+    else:
+        m = min(r_ls, key=lambda x: x[0])
+    #print(r_ls)
+    #if depth == 0:
+    #    print(depth, sorted(r_ls, key=lambda x: x[0], reverse=True), turn)
+    return m
+
+
+
 board_h = []
 
 board_v = []
@@ -99,16 +199,33 @@ for i in range(m-1):
         ls.append(e)
     board_v.append(ls)
 
-for i in range(n-1):
+for i in range(m-1):
     ls = []
-    for j in range(m-1):
-        edge_0 = board_h[j][i]
-        edge_1 = board_h[j+1][i]
-        edge_2 = board_v[j][i]
-        edge_3 = board_v[j][i+1]
-        square = Square(i, j, 0)
+    for j in range(n-1):
+        edge_0 = board_h[i][j]
+        edge_1 = board_h[i+1][j]
+        edge_2 = board_v[i][j]
+        edge_3 = board_v[i][j+1]
+        square = Square(j, i, 0)
         square.ls.extend([edge_0, edge_1, edge_2, edge_3])
         ls.append(square)
     board_p.append(ls)
 
-draw_board(board_h, board_v, board_p)
+i_x = 0
+i_y = 0
+ls_edges = []
+for i in range(len(board_h) + len(board_v)):
+    if i % 2 == 0:
+        ls_edges.append(board_h[i_x])
+        i_x += 1
+    else:
+        ls_edges.append(board_v[i_y])
+        i_y += 1
+
+board_h[0][0].c = 1
+board_h[0][1].c = 1
+board_v[0][1].c = 1
+board_v[0][2].c = 1
+
+draw_board(ls_edges, board_p)
+print(calculate_reward_regret_0_layer(ls_edges, board_p))
